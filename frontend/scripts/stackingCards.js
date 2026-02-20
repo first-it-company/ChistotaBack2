8e73352e34aafb33ebeb27b880857e52c70d3935
kuren <aleksandrapettrova@gmail.com>
2026-02-20 10:40:43 +1000

    some front fixes

--- frontend/scripts/stackingCards.js
+++ frontend/scripts/stackingCards.js
@@ -8,47 +8,36 @@ export function initStackingCards() {
     const cards = wrapper.querySelectorAll('[data-stacking-cards-item]');
     if (cards.length === 0) return;
 
-    const getCardHeight = () => {
-        return cards[0].offsetHeight;
-    };
-
     const CARD_GAP = 30;
-    const TOP_OFFSET = 45;
+    const TOP_OFFSET = 80;
     const scaleStep = 0.025;
     const cardCount = cards.length;
 
+    let triggers = [];
+    let resizeTimer;
+
+    const killAll = () => {
+        triggers.forEach(t => t?.kill());
+        triggers = [];
+        gsap.set(cards, { clearProps: 'transform' });
+        wrapper.style.minHeight = '';
+    };
+
     const setupAnimation = () => {
-        const CARD_HEIGHT = getCardHeight();
+        const CARD_HEIGHT = cards[0].offsetHeight;
 
-        gsap.set(cards, {
-            willChange: 'transform',
-            force3D: true,
-        });
+        const totalHeight =
+            CARD_HEIGHT * cardCount +
+            CARD_GAP * (cardCount - 1) +
+            CARD_HEIGHT;
 
-        const totalHeight = (CARD_HEIGHT * cardCount) + (CARD_GAP * (cardCount - 1)) + CARD_HEIGHT;
         wrapper.style.minHeight = `${totalHeight}px`;
-
-        const triggers = [];
 
         cards.forEach((card, i) => {
             const reverseIndex = cardCount - i;
-            const targetScale = 1 - (reverseIndex * scaleStep);
-            const startOffset = TOP_OFFSET + (i * CARD_GAP);
-            const pinDuration = CARD_HEIGHT * reverseIndex
-
-            const scaleTween = gsap.to(card, {
-                scrollTrigger: {
-                    trigger: card,
-                    start: "top center",
-                    end: "bottom center",
-                    scrub: true,
-                    invalidateOnRefresh: true,
-                },
-                ease: "none",
-                scale: targetScale,
-                force3D: true,
-                transformOrigin: "center center",
-            });
+            const targetScale = 1 - reverseIndex * scaleStep;
+            const startOffset = TOP_OFFSET + i * CARD_GAP;
+            const pinDuration = CARD_HEIGHT * reverseIndex;
 
             const pinTrigger = ScrollTrigger.create({
                 trigger: card,
@@ -59,24 +48,41 @@ export function initStackingCards() {
                 invalidateOnRefresh: true,
             });
 
-            triggers.push(scaleTween.scrollTrigger, pinTrigger);
+            const scaleTween = gsap.to(card, {
+                scrollTrigger: {
+                    trigger: card,
+                    start: `top top+=${startOffset}px`,
+                    end: `+=${pinDuration}px`,
+                    scrub: 0.5,
+                    invalidateOnRefresh: true,
+                },
+                ease: "none",
+                scale: targetScale,
+                force3D: true,
+                transformOrigin: "top center",
+            });
+
+            triggers.push(pinTrigger, scaleTween.scrollTrigger);
         });
 
-        return triggers;
+        ScrollTrigger.refresh();
     };
 
-    let triggers = setupAnimation();
+    setupAnimation();
 
     const resizeObserver = new ResizeObserver(() => {
-        triggers.forEach(trigger => trigger?.kill());
-        ScrollTrigger.refresh();
-        triggers = setupAnimation();
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(() => {
+            killAll();
+            setupAnimation();
+        }, 100);
     });
 
-    resizeObserver.observe(cards[0]);
+    resizeObserver.observe(wrapper);
 
     return () => {
-        triggers.forEach(trigger => trigger?.kill());
+        clearTimeout(resizeTimer);
+        killAll();
         resizeObserver.disconnect();
         gsap.set(cards, { clearProps: 'all' });
     };
